5139b6c06bce92dc664e7a5cf528beb84ba479c0
Jack.cai <Jackcai@emobility.com.ng>
2025-06-20 18:06:07 +0800

    feat：修改脚本文件

--- matrix/matrix-iOS/Script/ks2apple.py
+++ matrix/matrix-iOS/Script/ks2apple.py
@@ -7,108 +7,174 @@
 @LastAuthor: Do not edit
 '''
 
-
+import html
 import json
 import os
 import sys
-import cgi
+# import html
 import optparse
 import traceback
 from datetime import datetime
 
 
-reload(sys)
-sys.setdefaultencoding('utf-8')
+# reload(sys)
+# sys.setdefaultencoding('utf-8')
 
 device_map = {
-    'iPod1,1' : 'iPod touch 1G',
-    'iPod2,1' : 'iPod touch 2G',
-    'iPod3,1' : 'iPod touch 3G',
-    'iPod4,1' : 'iPod touch 4G',
-    'iPod5,1' : 'iPod touch 5G',
+    # iPod touch
+    'iPod1,1': 'iPod touch 1G',
+    'iPod2,1': 'iPod touch 2G',
+    'iPod3,1': 'iPod touch 3G',
+    'iPod4,1': 'iPod touch 4G',
+    'iPod5,1': 'iPod touch 5G',
+    'iPod7,1': 'iPod touch 6G',
+    'iPod9,1': 'iPod touch 7G',
 
-    'iPad2,5' : 'iPad mini 1G',
-    'iPad2,6' : 'iPad mini 1G',
-    'iPad2,7' : 'iPad mini 1G',
-    'iPad4,4' : 'iPad mini 2G',
-    'iPad4,5' : 'iPad mini 2G',
-    'iPad4,6' : 'iPad mini 2G',
-    'iPad4,7' : 'iPad mini 3',
-    'iPad4,8' : 'iPad mini 3',
-    'iPad4,9' : 'iPad mini 3',
-    'iPad5,1' : 'iPad mini 4',
-    'iPad5,2' : 'iPad mini 4',
+    # iPad mini
+    'iPad2,5': 'iPad mini 1G',
+    'iPad2,6': 'iPad mini 1G',
+    'iPad2,7': 'iPad mini 1G',
+    'iPad4,4': 'iPad mini 2G',
+    'iPad4,5': 'iPad mini 2G',
+    'iPad4,6': 'iPad mini 2G',
+    'iPad4,7': 'iPad mini 3',
+    'iPad4,8': 'iPad mini 3',
+    'iPad4,9': 'iPad mini 3',
+    'iPad5,1': 'iPad mini 4',
+    'iPad5,2': 'iPad mini 4',
+    'iPad11,1': 'iPad mini 5',
+    'iPad11,2': 'iPad mini 5',
+    'iPad14,1': 'iPad mini 6',
+    'iPad14,2': 'iPad mini 6',
 
-    'iPad1,1' : 'iPad 1G',
-    'iPad2,1' : 'iPad 2',
-    'iPad2,2' : 'iPad 2',
-    'iPad2,3' : 'iPad 2',
-    'iPad2,4' : 'iPad 2',
-    'iPad3,1' : 'iPad 3',
-    'iPad3,2' : 'iPad 3',
-    'iPad3,3' : 'iPad 3',
-    'iPad3,4' : 'iPad 4',
-    'iPad3,5' : 'iPad 4',
-    'iPad3,6' : 'iPad 4',
-    'iPad4,1' : 'iPad Air',
-    'iPad4,2' : 'iPad Air',
-    'iPad4,3' : 'iPad Air',
-    'iPad5,3' : 'iPad Air 2',
-    'iPad5,4' : 'iPad Air 2',
-    'iPad6,3' : 'iPad Pro 9.7-inch',
-    'iPad6,4' : 'iPad Pro 9.7-inch',
-    'iPad6,7' : 'iPad Pro 12.9-inch',
-    'iPad6,8' : 'iPad Pro 12.9-inch',
-    'iPad6,11' : 'iPad 5',
-    'iPad6,12' : 'iPad 5',
-    'iPad7,11' : 'iPad 6',
-    'iPad7,12' : 'iPad 6',
-    'iPad7,1' : 'iPad Pro 12.9-inch 2',
-    'iPad7,2' : 'iPad Pro 12.9-inch 2',
-    'iPad7,3' : 'iPad Pro 10.5-inch',
+    # iPad
+    'iPad1,1': 'iPad 1G',
+    'iPad2,1': 'iPad 2',
+    'iPad2,2': 'iPad 2',
+    'iPad2,3': 'iPad 2',
+    'iPad2,4': 'iPad 2',
+    'iPad3,1': 'iPad 3',
+    'iPad3,2': 'iPad 3',
+    'iPad3,3': 'iPad 3',
+    'iPad3,4': 'iPad 4',
+    'iPad3,5': 'iPad 4',
+    'iPad3,6': 'iPad 4',
+    'iPad6,11': 'iPad 5',
+    'iPad6,12': 'iPad 5',
+    'iPad7,5': 'iPad 6',
+    'iPad7,6': 'iPad 6',
+    'iPad7,11': 'iPad 7',
+    'iPad7,12': 'iPad 7',
+    'iPad11,6': 'iPad 8',
+    'iPad11,7': 'iPad 8',
+    'iPad12,1': 'iPad 9',
+    'iPad12,2': 'iPad 9',
+    'iPad13,18': 'iPad 10',
+    'iPad13,19': 'iPad 10',
 
+    # iPad Air
+    'iPad4,1': 'iPad Air',
+    'iPad4,2': 'iPad Air',
+    'iPad4,3': 'iPad Air',
+    'iPad5,3': 'iPad Air 2',
+    'iPad5,4': 'iPad Air 2',
+    'iPad11,3': 'iPad Air 3',
+    'iPad11,4': 'iPad Air 3',
+    'iPad13,1': 'iPad Air 4',
+    'iPad13,2': 'iPad Air 4',
+    'iPad13,16': 'iPad Air 5',
+    'iPad13,17': 'iPad Air 5',
 
-    'iPhone1,1' : 'iPhone',
-    'iPhone1,2' :  'iPhone 3G',
-    'iPhone2,1' : 'iPhone 3GS',
-    'iPhone3,1' : 'iPhone 4',
-    'iPhone3,2' : 'iPhone 4',
-    'iPhone3,3' : 'iPhone 4',
-    'iPhone4,1' : 'iPhone 4s',
-    'iPhone5,1' : 'iPhone 5',
-    'iPhone5,2' : 'iPhone 5',
-    'iPhone5,3' : 'iPhone 5c',
-    'iPhone5,4' : 'iPhone 5c',
-    'iPhone6,1' : 'iPhone 5s',
-    'iPhone6,2' : 'iPhone 5s',
+    # iPad Pro
+    'iPad6,3': 'iPad Pro 9.7-inch',
+    'iPad6,4': 'iPad Pro 9.7-inch',
+    'iPad6,7': 'iPad Pro 12.9-inch',
+    'iPad6,8': 'iPad Pro 12.9-inch',
+    'iPad7,1': 'iPad Pro 12.9-inch 2nd Gen',
+    'iPad7,2': 'iPad Pro 12.9-inch 2nd Gen',
+    'iPad7,3': 'iPad Pro 10.5-inch',
+    'iPad7,4': 'iPad Pro 10.5-inch',
+    'iPad8,1': 'iPad Pro 11-inch 1st Gen',
+    'iPad8,2': 'iPad Pro 11-inch 1st Gen',
+    'iPad8,3': 'iPad Pro 11-inch 1st Gen',
+    'iPad8,4': 'iPad Pro 11-inch 1st Gen',
+    'iPad8,5': 'iPad Pro 12.9-inch 3rd Gen',
+    'iPad8,6': 'iPad Pro 12.9-inch 3rd Gen',
+    'iPad8,7': 'iPad Pro 12.9-inch 3rd Gen',
+    'iPad8,8': 'iPad Pro 12.9-inch 3rd Gen',
+    'iPad8,9': 'iPad Pro 11-inch 2nd Gen',
+    'iPad8,10': 'iPad Pro 11-inch 2nd Gen',
+    'iPad8,11': 'iPad Pro 12.9-inch 4th Gen',
+    'iPad8,12': 'iPad Pro 12.9-inch 4th Gen',
+    'iPad13,4': 'iPad Pro 11-inch 3rd Gen',
+    'iPad13,5': 'iPad Pro 11-inch 3rd Gen',
+    'iPad13,6': 'iPad Pro 11-inch 3rd Gen',
+    'iPad13,7': 'iPad Pro 11-inch 3rd Gen',
+    'iPad13,8': 'iPad Pro 12.9-inch 5th Gen',
+    'iPad13,9': 'iPad Pro 12.9-inch 5th Gen',
+    'iPad13,10': 'iPad Pro 12.9-inch 5th Gen',
+    'iPad13,11': 'iPad Pro 12.9-inch 5th Gen',
+    'iPad14,3': 'iPad Pro 11-inch 4th Gen',
+    'iPad14,4': 'iPad Pro 11-inch 4th Gen',
+    'iPad14,5': 'iPad Pro 12.9-inch 6th Gen',
+    'iPad14,6': 'iPad Pro 12.9-inch 6th Gen',
 
-    'iPhone7,1' : 'iPhone 6 Plus',
-    'iPhone7,2' : 'iPhone 6',
-    'iPhone8,1' : 'iPhone 6s',
-    'iPhone8,2' : 'iPhone 6s Plus',
-    'iPhone8,4' : 'iPhone SE',
-    'iPhone9,1' : 'iPhone 7',
-    'iPhone9,3' : 'iPhone 7',
-    'iPhone9,2' : 'iPhone 7 Plus',
-    'iPhone9,4' : 'iPhone 7 Plus',
-    'iPhone10,1' : 'iPhone 8',
-    'iPhone10,4' : 'iPhone 8',
-    'iPhone10,2' : 'iPhone 8 Plus',
-    'iPhone10,5' : 'iPhone 8 Plus',
-    'iPhone10,3' : 'iPhone X',
-    'iPhone10,6' : 'iPhone X',
-    'iPhone11,2' : 'iPhone XS',
-    'iPhone11,4' : 'iPhone XS Max',
-    'iPhone11,6' : 'iPhone XS Max',
-    'iPhone11,8' : 'iPhone XR',
-    'iPhone12,1' : 'iPhone 11',
-    'iPhone12,3' : 'iPhone 11 Pro',
-    'iPhone12,5' : 'iPhone 11 Pro Max',
-    'iPhone13,1' : 'iPhone 12 mini',
-    'iPhone13,2' : 'iPhone 12',
-    'iPhone13,3' : 'iPhone 12 Pro',
-    'iPhone13,4' : 'iPhone 12 Pro Max',
+    # iPhone
+    'iPhone1,1': 'iPhone 1G',
+    'iPhone1,2': 'iPhone 3G',
+    'iPhone2,1': 'iPhone 3GS',
+    'iPhone3,1': 'iPhone 4',
+    'iPhone3,2': 'iPhone 4',
+    'iPhone3,3': 'iPhone 4',
+    'iPhone4,1': 'iPhone 4S',
+    'iPhone5,1': 'iPhone 5',
+    'iPhone5,2': 'iPhone 5',
+    'iPhone5,3': 'iPhone 5c',
+    'iPhone5,4': 'iPhone 5c',
+    'iPhone6,1': 'iPhone 5s',
+    'iPhone6,2': 'iPhone 5s',
+    'iPhone7,1': 'iPhone 6 Plus',
+    'iPhone7,2': 'iPhone 6',
+    'iPhone8,1': 'iPhone 6s',
+    'iPhone8,2': 'iPhone 6s Plus',
+    'iPhone8,4': 'iPhone SE 1st Gen',
+    'iPhone9,1': 'iPhone 7',
+    'iPhone9,3': 'iPhone 7',
+    'iPhone9,2': 'iPhone 7 Plus',
+    'iPhone9,4': 'iPhone 7 Plus',
+    'iPhone10,1': 'iPhone 8',
+    'iPhone10,4': 'iPhone 8',
+    'iPhone10,2': 'iPhone 8 Plus',
+    'iPhone10,5': 'iPhone 8 Plus',
+    'iPhone10,3': 'iPhone X',
+    'iPhone10,6': 'iPhone X',
+    'iPhone11,2': 'iPhone XS',
+    'iPhone11,4': 'iPhone XS Max',
+    'iPhone11,6': 'iPhone XS Max',
+    'iPhone11,8': 'iPhone XR',
+    'iPhone12,1': 'iPhone 11',
+    'iPhone12,3': 'iPhone 11 Pro',
+    'iPhone12,5': 'iPhone 11 Pro Max',
+    'iPhone12,8': 'iPhone SE 2nd Gen',
+    'iPhone13,1': 'iPhone 12 mini',
+    'iPhone13,2': 'iPhone 12',
+    'iPhone13,3': 'iPhone 12 Pro',
+    'iPhone13,4': 'iPhone 12 Pro Max',
+    'iPhone14,4': 'iPhone 13 mini',
+    'iPhone14,5': 'iPhone 13',
+    'iPhone14,2': 'iPhone 13 Pro',
+    'iPhone14,3': 'iPhone 13 Pro Max',
+    'iPhone14,6': 'iPhone SE 3rd Gen',
+    'iPhone14,7': 'iPhone 14',
+    'iPhone14,8': 'iPhone 14 Plus',
+    'iPhone15,2': 'iPhone 14 Pro',
+    'iPhone15,3': 'iPhone 14 Pro Max',
+    'iPhone15,4': 'iPhone 15',
+    'iPhone15,5': 'iPhone 15 Plus',
+    'iPhone16,1': 'iPhone 15 Pro',
+    'iPhone16,2': 'iPhone 15 Pro Max',
 }
+
 
 def dump_json(obj):
     return json.dumps(obj, indent=4,
@@ -294,7 +360,7 @@ def parse_system_info(report):
 
     headers = ['System Info: {']
     device = _s('machine')
-    if device_map.has_key(device):
+    if device in device_map:
         device = device_map[device]
     headers.append('    Device:      {0}'.format(device))
     headers.append('    OS Version:  {0} {1} ({2})'.format(_s('system_name'), _s('system_version'), _s('os_version')))
@@ -348,7 +414,8 @@ def parse_error_info(report):
         return [] 
     error = crash.get('error')
     if not error:
-        print "waring: no error found in crash"
+
+        print("warning: no error found in crash")
         return []
     mach = error['mach']
     signal = error['signal']
@@ -391,7 +458,8 @@ def parse_crash_reason(report):
 
     error = crash.get('error')
     if not error:
-        print "warning: no error found in crash"
+
+        print("warning: no error found in crash")
         return []
     crash_type = error['type']
 
@@ -435,7 +503,7 @@ def parse_backtrace(report, backtrace):
         
         img = get_belong_img(report, pc)
         if not img:
-            print "error, no img found for pc: %s" % pc
+            print("error, no img found for pc: %s" % pc)
             result.append('{0:<4}{1:31} 0x{2:016x}'.format(num, 'unknown', pc))
             num += 1
             continue
@@ -594,7 +662,7 @@ def parse_binary_images(report):
                             uuid, path))
             else:
                 if name == 'WeChat':
-                    print '{0:>#18x} - {1:>#18x} {2}{3}  <{4}> {5}'.format(addr, addr+size-1, is_base, name,uuid, path)
+                    print('{0:>#18x} - {1:>#18x} {2}{3}  <{4}> {5}'.format(addr, addr+size-1, is_base, name, uuid, path))
                 result.append('{0:>#18x} - {1:>#18x} {2}{3}  <{4}> {5}'\
                     .format(addr, addr+size-1, is_base, name,
                             uuid, path))
@@ -765,7 +833,8 @@ def parse_other_info(report):
     result = ['']
     user_info = report.get("user", None)
     if not user_info or not user_info.get(APP_NAME, None) or not user_info[APP_NAME].get("log"):
-        print "no log found..."
+
+        print("no log found...")
         return result 
 
     log_list = user_info[APP_NAME]["log"][-2:]
@@ -788,7 +857,7 @@ def ks_json_2_apple(report, fout):
         traceback.print_exc()
         errors = []
     for line in errors:
-        line = cgi.escape(line)
+        line = html.escape(line)
         fout.write(line+'\n')
 
     try:
@@ -797,7 +866,7 @@ def ks_json_2_apple(report, fout):
         traceback.print_exc()
         reason = []
     for line in reason:
-        line = cgi.escape(line)
+        line = html.escape(line)
         fout.write(line+'\n')
 
     try:
@@ -826,32 +895,32 @@ def ks_json_2_apple(report, fout):
     #    fout.write(line+'\n')
     click_info = parse_click_info(report)
     for line in click_info:
-        line = cgi.escape(line)
+        line = html.escape(line)
         fout.write(line+'\n')
     
     threads = parse_thread_list(report)
     for line in threads:
-        line = cgi.escape(line)
+        line = html.escape(line)
         fout.write(line+'\n')
 
     cpu_state = parse_cpu_state(report)
     for line in cpu_state:
-        line = cgi.escape(line)
+        line = html.escape(line)
         fout.write(line+'\n')
 
     images = parse_binary_images(report)
     for line in images:
-        #line = cgi.escape(line)
+        #line = html.escape(line)
         fout.write(line+'\n')
 
     extra = parse_extra_info(report)
     for line in extra:
-        line = cgi.escape(line)
+        line = html.escape(line)
         fout.write(line+'\n')
     
     log_info = parse_log_info(report)
     for line in log_info:
-        line = cgi.escape(line)
+        line = html.escape(line)
         fout.write(line+'\n')
     
 if __name__ == '__main__':
